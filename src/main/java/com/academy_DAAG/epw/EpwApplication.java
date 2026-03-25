d7e6a6f8944b7bb3cc1663fa45937d4040a2a764
AguirreA6 <hola@pu.com>
2026-03-25 07:28:10 -0500

    first commit

---
+++ src/main/java/com/academy_DAAG/epw/EpwApplication.java
@@ -0,0 +1,13 @@
+package com.academy_DAAG.epw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy_DAAG/epw/EpwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy_DAAG.epw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

144fd66be4222b93a8ee24c1274ab4ff1a61eb24
AguirreA6 <hola@pu.com>
2026-03-25 07:53:43 -0500

    first commit

--- src/main/java/com/academy_DAAG/epw/EpwApplication.java
+++ src/main/java/com/academy_DAAG/epw/EpwApplication.java
@@ -2,9 +2,17 @@ package com.academy_DAAG.epw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EpwApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World! Spring Boot 4 está aquí.";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpwApplication.class, args);
